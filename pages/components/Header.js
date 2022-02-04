5108b61f29bcdd5a79d98955bd7fd2bad22c2072
emon1995 <emonhossain995@gmail.com>
2022-02-04 19:46:23 +0600

    Header component complete

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,6 +1,28 @@
+import styles from '../../styles/Home.module.css';
+import Image from 'next/image';
+import proPic from '../../images/watermark.png';
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className={styles.headcontainer}>
+      <div className={styles.headwrapper}>
+        <div className={styles.title}>
+          <h2>
+            Hello, <span>Emon</span>
+          </h2>
+          <p>Welcome to the board</p>
+        </div>
+        <div className={styles.profile}>
+          <Image
+            className={styles.image}
+            src={proPic}
+            alt='profile'
+            width='40px'
+            height='40px'
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
-
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from './components/Header'
 import LeftNavbar from './components/LeftNavbar'
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       {/* leftNavbar */}
       <LeftNavbar />
       {/* Header */}
+      <Header />
       {/* Content */}
     </div>
   )
